f462ef3c42472996c572086e5b3ddb08114ed406
michellel <michellel@splunk.com>
2018-09-25 14:59:04 -0700

    PAPP-4988

--- ewsonprem_connector.py
+++ ewsonprem_connector.py
@@ -1,16 +1,8 @@
-# --
-# File: ewsonprem_connector.py
+# Copyright (c) 2016-2018 Splunk Inc.
 #
-# Copyright (c) Phantom Cyber Corporation, 2016-2018
+# SPLUNK CONFIDENTIAL – Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
 #
-# This unpublished material is proprietary to Phantom Cyber.
-# All rights reserved. The methods and
-# techniques described herein are considered trade secrets
-# and/or confidential. Reproduction or distribution, in whole
-# or in part, is forbidden except by express written permission
-# of Phantom Cyber.
-#
-# --
 
 # ==========================================================
 # To Grant access to a user's mailbox to another, read the command found at the following location
