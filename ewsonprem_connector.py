a5343b96cbee8c2cbd688bbb174a38356c20dca6
jpatel <jpatel@splunk.com>
2019-05-02 19:51:33 +0530

    EWSOffice365: Made value of PRIMARY field 'true' wherever required, corrected spelling mistakes

--- ewsonprem_connector.py
+++ ewsonprem_connector.py
@@ -2318,7 +2318,7 @@ if __name__ == '__main__':
             r2 = requests.post(BaseConnector._get_phantom_base_url() + "login", verify=False, data=data, headers=headers)
             session_id = r2.cookies['sessionid']
         except Exception as e:
-            print ("Unable to get session id from the platfrom. Error: " + str(e))
+            print ("Unable to get session id from the platform. Error: " + str(e))
             exit(1)
 
     with open(args.input_test_json) as f:
